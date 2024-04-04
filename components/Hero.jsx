ef37ed79d6177fdcd62d40d373911fd59ffbc6cf
KHANSAB <mohsin.khansab@capgemini.com>
2024-04-04 11:48:28 +0530

    modify structure of page

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,26 +4,28 @@ import { getImageUrl } from './imgs.js';
 
 function Tile(props) {
     return(
-        <div className="tile--component">
-            <div className="tile--img"><img src={getImageUrl(props.img)}/></div>
-            <div className="tile--content">
-                <div className="tileContent--loc">
-                    <img className='tileContent--loc--img' src={locLogo} />
-                    <p className="tileContent--loc--country">{props.location}</p>
-                    <a href={props.googleMapsUrl}>View on Google Maps</a>
-                </div>
-                <div className="tileContent--title">
-                    <p>{props.title}</p>
-                </div>
-                <div className="tileContent--date">
-                    <h4>{props.startDate} - {props.endDate}</h4>
+        <>
+            <div className="tile--component">
+                <div className="tile--img"><img src={getImageUrl(props.img)}/></div>
+                <div className="tile--content">
+                    <div className="tileContent--loc">
+                        <img className='tileContent--loc--img' src={locLogo} />
+                        <p className="tileContent--loc--country">{props.location}</p>
+                        <a href={props.googleMapsUrl}>View on Google Maps</a>
+                    </div>
+                    <div className="tileContent--title">
+                        <p>{props.title}</p>
+                    </div>
+                    <div className="tileContent--date">
+                        <h4>{props.startDate} - {props.endDate}</h4>
+                    </div>
                 </div>
                 <div className="tileContent--text">
                     <p>{props.description}</p>
                 </div>
-                <hr />
             </div>
-        </div>
+            <hr />
+        </>
     )
 }
 
